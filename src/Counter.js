422e7c85f108d3150455879a19ec112234e3f72a
Minoru Miyata <miyarappo@gmail.com>
2019-06-02 21:40:01 +0900

    Implement counter component

---
+++ src/Counter.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+
+const Counter = () => {
+  const [ count, setCount ] = useState(0);
+  return (
+    <div>
+      <div>You clicked { count } times</div>
+      <button onClick={() => setCount(count + 1)}>+ 1</button>
+      <button onClick={() => setCount(count -1)}>- 1</button>
+    </div>
+  )
+}
+
+export default Counter;
